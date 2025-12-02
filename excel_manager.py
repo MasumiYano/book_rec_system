7386dbe9023139e0cdffa7064c1bacffc1dfcc09
ag0021 <kaikii@fpt.com>
2025-12-02 07:04:16 +0000

    formatter for excel improved

--- excel_manager.py
+++ excel_manager.py
@@ -1,4 +1,4 @@
-from openpyxl import Workbook
+from openpyxl import load_workbook 
 import pandas as pd
 import os
 
@@ -6,24 +6,29 @@ import os
 class ExcelManager():
     def __init__(self, kagge_df: pd.DataFrame, excel_path: str):
         self.dataframe = kagge_df.head(100)
-        self.workbook = Workbook()
-        self.sheet = self.workbook.active
         self.excel_path = excel_path
 
     def to_excel (self):
         os.makedirs(self.excel_path, exist_ok=True)
         file_path = os.path.join(self.excel_path, "data.xlsx")
         try: 
-            with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
-                self.dataframe.to_excel(writer, sheet_name='Sheet1', index=False)
-                
-                workbook = writer.book
-                worksheet = writer.sheets['Sheet1']
-
-                worksheet.column_dimensions['A'].width = 15
-                worksheet.column_dimensions['B'].width = 20
-
-                print("Sucess")
+            self.dataframe.to_excel(f"{file_path}", index=False, engine='openpyxl')
+            self.format_excel(self.dataframe)
 
         except (FileNotFoundError, Exception) as e:
             print(f"An error ocurred: {e}")
+
+    def format_excel(self, excel_file: pd.ExcelFile):
+        wb = load_workbook(f"{excel_file}")
+        ws = wb.active
+
+        for col in ws.columns:
+            max_length = 0
+            col_letter = col[0].column_letter
+            for cell in col:
+                if cell.value:
+                    max_length = max(max_length, len(str(cell.value)))
+
+        ws.column_dimensions[col_letter].width = max_length + 2
+
+        wb.save(f"{excel_file}")
